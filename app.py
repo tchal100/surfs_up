81338621be7e0943823ad7f46dc49fdd9b6fe52f
tchal100 <tchal100@gmail.com>
2021-02-15 19:16:54 -0500

    up date files

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask 
+from flask import Flask
 app = Flask(__name__)
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello world'
-    export FLASK_APP=app.py
-    set FLASK_APP=app.py
-    flask run 
+    return "Hello, World!"
+export FLASK_APP=app.py
+
+if_name _=="_name_":
+    app.run(hosts="0.0.0.0" debug=true, port=5000)
